2db1e11461a0c247a05b89a1aa0be512c10b5592
trtd56 <hoge@fuga.com>
2016-06-26 13:10:44 +0900

    Add simple python calculate script by using TesorFlow

---
+++ sample02.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+This is same program.
+'''
+
+import tensorflow as tf
+
+if __name__=="__main__":
+    # make constant value "x", and input "1" to "x".
+    x = tf.constant(1, name='x')
+    # make "y" variable, and "y" is defined "x + 2" 
+    y = tf.Variable(x + 2, name='y')
+    # initialize all variable
+    model = tf.initialize_all_variables()
+    # make session to run calculate
+    with tf.Session() as session:
+        # run model made line 16
+        session.run(model)
+        # run y
+        result = session.run(y)
+        # print result
+        print result
